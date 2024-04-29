59f4c90da8129dea9a0b5168a51b150d8971e37d
uthmanshadow <89521169+uthmanshadow@users.noreply.github.com>
2024-04-29 16:04:40 +0100

    Done

---
+++ menu.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Menu</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="project_details.php">Project Details</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project_progress.php">Project Progress</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="project_report.php">Project Report</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
---
+++ project_report.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Project Report</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="background: #eee;">
+
+<div class="container">
+  <h1 class="mt-5">Project Report</h1>
+  <table class="table table-bordered mt-4">
+    <thead>
+      <tr>
+        <th scope="col">S/N</th>
+        <th scope="col">Project Name</th>
+        <th scope="col">Department</th>
+        <th scope="col">Award Start Date</th>
+        <th scope="col">Project End Date</th>
+        <th scope="col">Task to Date</th>
+        <th scope="col">Task Remaining</th>
+        <th scope="col">Constraints</th>
+        <th scope="col">Remark</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Project A</td>
+        <td>Engineering</td>
+        <td>2024-04-26</td>
+        <td>2024-10-31</td>
+        <td>50%</td>
+        <td>50%</td>
+        <td>No major constraints</td>
+        <td>On track</td>
+      </tr>
+      
+    </tbody>
+  </table>
+</div>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
